3aa391c318580db9509e4110b115dda26d1106d7
JangHyeonJun <totok682@naver.com>
2024-08-29 10:18:08 +0900

    feat: 계좌 업데이트

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ export const WEDDING_DATE = "2024년 12월 01일, 일요일 오후 12시 40분";
 export const WEDDING_LOCATION = "영등포 더컨벤션, 1층 그랜드볼룸";
 
 export const GROOM_NAME = "장현준";
-export const GROOM_ACCOUNT_NUMBER = "국민은행 669102-01-472320";
+export const GROOM_ACCOUNT_NUMBER = "국민은행 938002-00-467818";
 export const GROOM_FATHER_NAME = "장남수";
-export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "국민은행 675601-01-110738";
 export const GROOM_MOTHER_NAME = "이민기";
-export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "국민은행 675602-01-126691";
 
 export const BRIDE_NAME = "조혜인";
 export const BRIDE_ACCOUNT_NUMBER = "우리은행 1002-548-188267";
 export const BRIDE_FATHER_NAME = "조현석";
-export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "신한은행 110-424-957117";
 export const BRIDE_MOTHER_NAME = "박지영";
 export const BRIDE_MOTHER_ACCOUNT_NUMBER = "우리은행 711-149566-02-101";
